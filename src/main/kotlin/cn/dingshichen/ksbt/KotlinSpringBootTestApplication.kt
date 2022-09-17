a258c985377b68a4b8c7c75113c0b7c7ec98dd77
dingshichen <foreverhuiqiao@126.com>
2022-09-17 16:10:15 +0800

    清理

--- src/main/kotlin/cn/dingshichen/ksbt/KotlinSpringBootTestApplication.kt
+++ src/main/kotlin/cn/dingshichen/ksbt/KotlinSpringBootTestApplication.kt
@@ -1,11 +1,24 @@
 package cn.dingshichen.ksbt
 
+import cn.dingshichen.ksbt.dto.R
+import cn.dingshichen.ksbt.dto.fail
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @SpringBootApplication
 class KotlinSpringBootTestApplication
 
 fun main(args: Array<String>) {
     runApplication<KotlinSpringBootTestApplication>(*args)
+}
+
+@RestControllerAdvice
+class GlobalExceptionTranslator {
+
+    @ExceptionHandler(Throwable::class)
+    fun handleError(e: Throwable): R<*> {
+        return fail(e.localizedMessage)
+    }
 }
